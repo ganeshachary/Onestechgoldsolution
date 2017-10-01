f4a3245ad5ac5ea2478cfafdc58e95b73c555bf5
psganesh <acharya.ganesh2@gmail.com>
2017-10-01 20:24:20 +0530

    Add AsyncTask file

---
+++ app/src/main/java/com/onestechsolution/onestechgoldsolution/Asynctask/AddCustomerDetailsAsyncTask.java
@@ -0,0 +1,29 @@
+package com.onestechsolution.onestechgoldsolution.Asynctask;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+/**
+ * Created by psganesh on 01/10/17.
+ */
+
+public class AddCustomerDetailsAsyncTask extends AsyncTask<String,String,String> {
+
+    Context context;
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+}
