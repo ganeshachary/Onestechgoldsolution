87c6911a08266335fa614d1526b7094d2c64c3f5
psganesh <acharya.ganesh2@gmail.com>
2017-10-01 19:55:30 +0530

    Completed validation for customer

--- app/src/main/java/com/onestechsolution/onestechgoldsolution/Activity/AddNewCustomerDetails.java
+++ app/src/main/java/com/onestechsolution/onestechgoldsolution/Activity/AddNewCustomerDetails.java
@@ -1,10 +1,17 @@
 package com.onestechsolution.onestechgoldsolution.Activity;
 
+import android.app.DatePickerDialog;
+import java.util.Calendar;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 
@@ -15,8 +22,15 @@ import com.onestechsolution.onestechgoldsolution.Utilities.Utility;
 
 public class AddNewCustomerDetails extends AppCompatActivity {
 
-    EditText etName , etMobile , etEmail , etState , etCity , etPincode ,etComments;
+    EditText etName , etMobile , etEmail , etState , etCity , etPincode ,etComments ,etBirthDate ,etAnniversaryDate;
     EditText[] editTextsArray;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    private DatePickerDialog.OnDateSetListener mDateSetLisenerBirthDate;
+    private DatePickerDialog.OnDateSetListener mDateSetLisenerAnniversaryDate;
+
+
+
+
 
     CustomerDeatils customerDeatils;
     @Override
@@ -35,7 +49,7 @@ public class AddNewCustomerDetails extends AppCompatActivity {
 
     void setUpUIReferrence()
     {
-        editTextsArray = new EditText[7];
+        editTextsArray = new EditText[9];
         etName = (EditText) findViewById(R.id.et_Name_AddCustomerDetailsActivity);
         editTextsArray[0] = etName;
 
@@ -56,6 +70,71 @@ public class AddNewCustomerDetails extends AppCompatActivity {
 
         etComments = (EditText) findViewById(R.id.et_Comments_AddCustomerDetailsActivity);
         editTextsArray[6] = etComments;
+
+        etBirthDate = (EditText) findViewById(R.id.et_BirthDate_AddCustomerDetailsActivity);
+        editTextsArray[7] = etBirthDate;
+
+        etAnniversaryDate =(EditText) findViewById(R.id.et_AnniversayDate_AddCustomerDetailsActivity);
+        editTextsArray[8] = etAnniversaryDate;
+
+
+        etBirthDate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Calendar calender = Calendar.getInstance();
+                int year = calender.get(Calendar.YEAR);
+                int month = calender.get(Calendar.MONTH);
+                int day = calender.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(AddNewCustomerDetails.this,android.R.style.Theme_Holo_Dialog_NoActionBar_MinWidth,mDateSetLisenerBirthDate
+                ,year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+
+            }
+        });
+
+        mDateSetLisenerBirthDate = new DatePickerDialog.OnDateSetListener(){
+
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month ,int day) {
+                month = month + 1;
+                String Date = day +"/"+month+"/"+year;
+                etBirthDate.setError(null);
+                etBirthDate.setText(Date);
+            }
+        };
+
+        etAnniversaryDate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Calendar calender = Calendar.getInstance();
+                int year = calender.get(Calendar.YEAR);
+                int month = calender.get(Calendar.MONTH);
+                int day = calender.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(AddNewCustomerDetails.this,android.R.style.Theme_Holo_Dialog_NoActionBar_MinWidth,mDateSetLisenerAnniversaryDate
+                        ,year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetLisenerAnniversaryDate = new DatePickerDialog.OnDateSetListener(){
+
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month ,int day) {
+                month = month + 1;
+                String Date = day +"/"+month+"/"+year;
+                etAnniversaryDate.setError(null);
+                etAnniversaryDate.setText(Date);
+
+            }
+        };
+
+
+
+
     }
 
 
@@ -83,9 +162,28 @@ public class AddNewCustomerDetails extends AppCompatActivity {
              ) {
             if((data = editText.getText().toString().trim()).isEmpty())
             {
+
                 Log.i("CHECK DATA",data);
                 editText.setError("Please fill this details");
                 vaildationState = false;
+            }
+            if(editText.getId() ==  R.id.et_Mobile_AddCustomerDetailsActivity)
+            {
+                if((data = editText.getText().toString().trim()).length() < 10)
+                {
+                    editText.setError("Please enter a valid mobile number");
+                    vaildationState = false;
+                }
+
+            }
+            if(editText.getId() ==  R.id.et_Email_AddCustomerDetailsActivity)
+            {
+                if(!android.util.Patterns.EMAIL_ADDRESS.matcher(editText.getText().toString().trim()).matches())
+                {
+                    editText.setError("Please enter a valid email id");
+                    vaildationState = false;
+                }
+
             }
 
         }
